f9dec55117923c8761a0e1bae7ac14a8261c1bf3
ronaldoavalos <ronaldo@vecinio.mx>
2023-11-24 15:46:13 -0600

    initial commit

--- loginScreenInstagramUIkit/ViewController.swift
+++ loginScreenInstagramUIkit/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    @IBAction func loginButtonAction() {
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        
+        if email == "ronaldo@vecinio.mx", password == "123"{
+            performSegue(withIdentifier: "home_segue", sender: nil)
+            
+        }else{
+            
+            
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
